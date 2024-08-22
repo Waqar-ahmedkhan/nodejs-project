b6ca123f6512628cc3345ba89f6e43cf3098b6f9
Waqar ahmed <waqarahmed44870@gmail.com>
2024-08-22 23:46:40 +0500

    commit

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,5 +1,10 @@
 import mongoose, { model, Schema } from "mongoose";
 
 const userMongoose = new mongoose.Schema({
-    
+    username: { type: "string", required: true },
+    password: { type: "string", required: true},
+    email: { type: "string", required: true, unique: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+
 })
