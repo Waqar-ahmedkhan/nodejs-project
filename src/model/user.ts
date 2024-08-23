058d3b82e0b9bad1411d3f25a3cd6cd93f4b6b66
Waqar ahmed <waqarahmed44870@gmail.com>
2024-08-23 23:18:41 +0500

    commit

--- src/model/user.ts
+++ src/model/user.ts
@@ -2,9 +2,15 @@ import mongoose, { model, Schema } from "mongoose";
 
 const userMongoose = new mongoose.Schema({
     username: { type: "string", required: true },
-    password: { type: "string", required: true},
-    email: { type: "string", required: true, unique: true },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now }
+    email: { type: "string", required: true},
+    authentication: {
+        password: { type: "string", required: true},
+        salt: { type:"string", required: false}, 
+        sessionToken: { type: "string", required:true },
+
+    }
+
 
 })
+
+export const userModel = mongoose.model("User", userMongoose)
